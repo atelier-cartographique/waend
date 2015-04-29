f36804acfcd424952e6f305d92046c2cea566cee
Pacome Beru <pacome.beru@gmail.com>
2015-04-29 16:45:00 +0200

    hint on get command

--- app/lib/commands/getAttribute.js
+++ app/lib/commands/getAttribute.js
@@ -14,6 +14,8 @@ var _ = require('underscore');
 
 function getAttr () {
     var self = this,
+        shell = self.shell,
+        stdout = shell.stdout,
         args = _.toArray(arguments),
         key = args.shift(),
         sys = self.sys,
@@ -29,7 +31,17 @@ function getAttr () {
             sys.stdout.write('"'+ key+'" : '+ JSON.stringify(data[key]));
         }
     }
+
+    stdout.write('You can add or edit any attribute of this element');
+    stdout.write('simply by writing :');
+    stdout.write('set attribute-name attribute-value');
+    stdout.write('Eg : ');
+    stdout.write('set name Brussels');
+    stdout.write('will give the name Brussels to your element');
+    stdout.write('Attributes are used to qualify your datas, like : age, height, number, etc.. ');
+
     return self.end(result);
+
 };
 
 
