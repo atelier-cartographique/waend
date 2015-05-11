a35c347e0cdf354ee1fb862a0db810329c07957a
Pierre Marchand <pierremarc07@gmail.com>
2015-05-11 17:32:03 +0200

    more verbose  importer

--- app/lib/commands/layer/importer.js
+++ app/lib/commands/layer/importer.js
@@ -15,62 +15,153 @@ var _ = require('underscore'),
     Geometry = require('../../Geometry');
 
 
+
 function setupDropZone (container) {
-    var dropbox = document.createElement('canvas');
-    dropbox.style.width = '100%';
-    dropbox.style.height = '100%';
-    dropbox.backgroundColor = 'transparent';
+    var dropbox = document.createElement('div'),
+        dropboxLabel = document.createElement('div');
+    dropbox.setAttribute('class', 'importer-dropzone');
+    dropboxLabel.setAttribute('class', 'importer-dropzone-label');
+
+    dropboxLabel.innerHTML = 'Drop Zone';
+
+    dropbox.appendChild(dropboxLabel);
     container.appendChild(dropbox);
     return dropbox;
 }
 
-function create (binder, uid, gid, lid, feature, batchJob) {
+function setupInput (container) {
+    var input = document.createElement('input'),
+        inputWrapper = document.createElement('div');
+    inputWrapper.setAttribute('class', 'importer-input-wrapper');
+    input.setAttribute('class', 'importer-input');
+    input.setAttribute('type', 'file');
+    container.appendChild(input);
+    return input;
+}
+
+
+function setupHints (container) {
+    var hints = document.createElement('div');
+    hints.setAttribute('class', 'importer-hints');
+    hints.innerHTML = [
+        '<p class="hint first-hint-line">drag and drop your GeoJSON file on map to import it</p>'
+        ,'<p class="hint">IMPORTANT :</p>'
+        ,'<p class="hint">Wænd currently does not support</p>'
+        ,'<p class="hint">multilines and multipolygons</p>'
+        ,'<p class="hint">nor points -yeah!-</p>'
+        ,'<p class="hint">Projection should be EPGS:4326 - WGS84</p>'
+    ].join(' ');
+    container.appendChild(hints);
+}
+
+
+function setupCancel (container) {
+    var cancel = document.createElement('div');
+    cancel.setAttribute('class', 'importer-cancel');
+    cancel.innerHTML = 'cancel';
+    container.appendChild(cancel);
+    return cancel;
+}
+
+
+
+var createData = {
+    'binder': null,
+    'uid': null,
+    'gid': null,
+    'lid': null
+};
+
+function setupCreateData (binder, uid, gid, lid) {
+    createData.binder = binder;
+    createData.uid = uid;
+    createData.gid = gid;
+    createData.lid = lid;
+}
+
+function create (feature, batchJob) {
     var olGeom = feature.getGeometry();
     var props = _.omit(feature.getProperties(), 'geometry', 'id');
     var geomType = olGeom.getType();
-    if(('Point' === geomType)
-        || ('LineString' === geomType)
-        || ('Polygon' === geomType)) {
-
+    if(('LineString' === geomType) || ('Polygon' === geomType)) {
         var geom = JSON.parse(Geometry.format.GeoJSON.write(olGeom));
         var data = {
-            'user_id': uid,
-            'layer_id': lid,
+            'user_id': createData.uid,
+            'layer_id': createData.lid,
             'properties': props,
             'geom': geom
         };
 
-        return binder.setFeature(uid, gid, lid, data, batchJob);
+        return createData.binder.setFeature(
+            createData.uid, createData.gid, createData.lid,
+            data, batchJob
+            );
     }
-    console.write('importer unsupported geom type', geomType);
+    console.error('importer unsupported geom type', geomType);
     return Promise.resolve();
 }
 
-function importer () {
-    var self = this,
-        shell = self.shell,
-        stdout = shell.stdout,
-        binder = self.binder,
-        terminal = shell.terminal,
-        map = shell.env.map,
-        current = self.current(),
-        uid = current[0],
-        gid = current[1],
-        lid = current[2],
-        display = terminal.display();
+function progress (length, index, options, feature) {
+    if (!options.progess) {
+        var container = options.container;
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        options.progress = document.createElement('div');
+        options.progress.total = document.createElement('div');
+        options.progress.counter = document.createElement('div');
+        options.progress.featureInfo = document.createElement('div');
 
-    stdout.write('<span class="hint first-hint-line">drag and drop your GeoJSON file on map to import it</span>');
-    stdout.write('<span class="hint">IMPORTANT :</span>');
-    stdout.write('<span class="hint">Wænd currently does not support</span>');
-    stdout.write('<span class="hint">multilines and multipolygons</span>');
-    stdout.write('<span class="hint">nor points -yeah!-</span>');
-    stdout.write('<span class="hint">Projection should be EPGS:4326 - WGS84</span>');
+        options.progress.setAttribute('class', 'importer-progress');
+        options.progress.total.setAttribute('class', 'importer-total');
+        options.progress.counter.setAttribute('class', 'importer-counter');
+        options.progress.featureInfo.setAttribute('class', 'importer-feature');
+
+        options.progress.total.innerHTML = length + ' features to import';
+
+        options.progress.appendChild(options.progress.total);
+        options.progress.appendChild(options.progress.counter);
+        options.progress.appendChild(options.progress.featureInfo);
+        container.appendChild(options.progress);
+    }
+
+    options.progress.counter.innerHTML = index.toString();
+    // var props = _.omit(feature.getProperties(), 'geometry');
+
+}
 
 
+var handleFile = function (file, options, resolve, reject) {
+    var reader = new FileReader();
 
-    var dropbox = setupDropZone(display.node);
+    var creator = function (evt) {
+        var geojson = evt.target.result,
+            features = Geometry.format.GeoJSON.readFeatures(geojson),
+            lastIndex = features.length - 1;
 
-    var resolver = function (resolve, reject) {
+        Promise.reduce(features, function(total, item, index, arrayLength){
+                var feature = features[index],
+                    lastOne = index === lastIndex;
+                progress (arrayLength, index, options, feature);
+                return create(feature, !lastOne);
+            }, 0)
+            .then(function(){
+                resolve();
+            })
+            .catch(reject)
+            .finally(function(){
+                options.display.end();
+            });
+
+    };
+    reader.onload = creator;
+    reader.readAsText(file);
+};
+
+function resolver (options) {
+    return function (resolve, reject) {
+
+        // Drag & Drop
         var dragenter = function (e) {
           e.stopPropagation();
           e.preventDefault();
@@ -88,42 +179,59 @@ function importer () {
           var dt = e.dataTransfer;
           var files = dt.files;
 
-          handleFile(files[0]);
+          handleFile(files[0], options.container,
+              resolve, reject);
         };
 
-        var handleFile = function (file) {
-            var reader = new FileReader();
+        options.dropbox.addEventListener("dragenter", dragenter, false);
+        options.dropbox.addEventListener("dragover", dragover, false);
+        options.dropbox.addEventListener("drop", drop, false);
 
-            var creator = function (evt) {
-                var geojson = evt.target.result,
-                    features = Geometry.format.GeoJSON.readFeatures(geojson),
-                    lastIndex = features.length - 1;
 
-                Promise.reduce(features, function(total, item, index){
-                        var feature = features[index],
-                            lastOne = index === lastIndex;
+        // Select
 
-                        return create(binder, uid, gid, lid, feature, !lastOne);
-                    }, 0)
-                    .then(function(){
-                        resolve();
-                    })
-                    .catch(reject)
-                    .finally(function(){
-                        display.end();
-                    });
+        options.input.addEventListener('change', function (e) {
+            (function () {
+                handleFile(options.input.files[0], options,
+                    resolve, reject);
+            })();
+        }, false);
 
-            };
-            reader.onload = creator;
-            reader.readAsText(file);
-        };
+        // Cancel
+        options.cancel.addEventListener('click', function () {
+            options.display.end();
+            reject('Cancel');
+        }, false);
 
-        dropbox.addEventListener("dragenter", dragenter, false);
-        dropbox.addEventListener("dragover", dragover, false);
-        dropbox.addEventListener("drop", drop, false);
     };
+}
 
-    return (new Promise(resolver));
+function importer () {
+    var self = this,
+        shell = self.shell,
+        stdout = shell.stdout,
+        binder = self.binder,
+        terminal = shell.terminal,
+        map = shell.env.map,
+        current = self.current(),
+        uid = current[0],
+        gid = current[1],
+        lid = current[2],
+        display = terminal.display();
+
+    setupCreateData(binder, uid, gid, lid);
+    setupHints(display.node);
+    var input = setupInput(display.node);
+    var dropbox = setupDropZone(display.node);
+    var cancel = setupCancel(display.node);
+    var options = {
+        'input': input,
+        'dropbox': dropbox,
+        'container': display.node,
+        'display': display,
+        'cancel': cancel
+    };
+    return (new Promise(resolver(options)));
 }
 
 
