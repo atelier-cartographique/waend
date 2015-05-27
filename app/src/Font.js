be99afca3e110dd2cce8d00adc64d779a9fd673c
Pierre Marchand <pierremarc07@gmail.com>
2015-05-27 17:53:04 +0200

    fixed too many font dowloads

--- app/src/Font.js
+++ app/src/Font.js
@@ -15,14 +15,30 @@ var config = require('../../config'),
 
 var FONT_URL = config.public.baseUrl + '/fonts/';
 
-var fonts = {};
+var fonts = {},
+    pendings = {};
+
+
+function processPendings (name) {
+    var ps = pendings[name],
+        f = fonts[name];
+    delete pendings[name];
+
+    for (var i = 0, l = ps.length; i < l; i++) {
+        var p = ps[i];
+        p[0].call(p[1], f);
+    }
+}
 
 module.exports.select = function (name, callback, ctx) {
     if(name in fonts) {
         callback.call(ctx, fonts[name]);
-        // return opentype.parse(fonts[name].buffer);
+    }
+    else if (name in pendings) {
+        pendings[name].push([callback, ctx]);
     }
     else {
+        pendings[name] = [[callback, ctx]];
         url = FONT_URL + name;
         opentype.load(url, function (err, f) {
             if (err) {
@@ -30,7 +46,7 @@ module.exports.select = function (name, callback, ctx) {
             }
             else {
                 fonts[name] = f;
-                callback.call(ctx, fonts[name]);
+                processPendings(name);
             }
         });
     }
