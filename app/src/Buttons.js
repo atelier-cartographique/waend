e6d0054d1555ee19823d9cabc65e80ea9a6b5ab4
Pacome Beru <pacome.beru@gmail.com>
2015-10-10 13:14:36 +0200

    added buttons to edit feature geometry

--- app/src/Buttons.js
+++ app/src/Buttons.js
@@ -14,7 +14,11 @@
      'set image': ['media pick | set params.image'],
      'set color': ['get color | edit | set style.strokeStyle'],
      'set text': ['get text | edit | set params.text'],
-     'go to feature' : ['gg | region set']
+     'edit geometry' : ['gg | trace | sg'],
+     're-draw feature' : ['draw | sg'],
+     're-trace feature' : ['trace | sg'],
+     'zoom to feature' : ['gg | region set']
+
  };
 
 var layerButtons = {
