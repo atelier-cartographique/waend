dd90c2f2f05d4e11765259feeea4cda02f10e361
Pacome Beru <pacome.beru@gmail.com>
2015-10-19 15:09:32 +0200

    moved scale on navigate and help wrapper

--- app/lib/commands/navigate.js
+++ app/lib/commands/navigate.js
@@ -415,7 +415,7 @@ Navigator.prototype.drawScale = function () {
 
     var left = 13,
         right = 74,
-        top = rect.height - 57,
+        top = rect.height - 17,
 	thickness = 6,
         bottom = top + thickness,
 	length = right - left,
