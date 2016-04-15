aeaba9f12a26577a1d800a7805205c89ca5450d8
Pierre Marchand <pierremarc07@gmail.com>
2016-04-15 17:30:58 +0200

    fixed permissions flaw

--- routes/endpoints/layer.js
+++ routes/endpoints/layer.js
@@ -17,103 +17,103 @@ var _ = require('underscore'),
 module.exports = exports = base.RequestHandler.extend({
 
 
-        endpoints: {
+    endpoints: {
 
-            list: {
-                verb: 'get',
-                handler: 'list',
-                url: 'user/:user_id/group/:group_id/layer/',
-                permissions: ['ownsGroupOrPublic']
-            },
-
-            get: {
-                verb: 'get',
-                handler: 'get',
-                url: 'user/:user_id/group/:group_id/layer/:layer_id',
-                permissions: ['ownsGroupOrPublic']
-            },
-
-            post: {
-                verb: 'post',
-                handler: 'post',
-                url: 'user/:user_id/group/:group_id/layer/',
-                permissions: ['isAuthenticated']
-            },
-
-            put: {
-                verb: 'put',
-                handler: 'put',
-                url: 'user/:user_id/group/:group_id/layer/:layer_id',
-                permissions: ['isAuthenticated', 'isUser', 'isGroupOwner']
-            }
+        list: {
+            verb: 'get',
+            handler: 'list',
+            url: 'user/:user_id/group/:group_id/layer/',
+            permissions: ['ownsGroupOrPublic']
         },
 
-
-        list: function (request, response) {
-            var self = this;
-            cache.client()
-                .getLayers(request.params.group_id)
-                .then(function(results){
-                    self.paginate(results, request, response);
-                })
-                .catch(function(err){
-                    response.status(500).send(err);
-                });
+        get: {
+            verb: 'get',
+            handler: 'get',
+            url: 'user/:user_id/group/:group_id/layer/:layer_id',
+            permissions: ['ownsGroupOrPublic']
         },
 
-        get: function (request, response) {
-            cache.client()
-                .get('layer', request.params.layer_id)
-                .then(function(data){
-                    response.send(data);
-                })
-                .catch(function(err){
-                    response.status(404).send(err);
-                });
+        post: {
+            verb: 'post',
+            handler: 'post',
+            url: 'user/:user_id/group/:group_id/layer/',
+            permissions: ['isAuthenticated', 'ownsGroupOrPublic']
         },
 
-
-        post: function (request, response) {
-            var groupId = request.params.group_id,
-                body = _.extend(request.body, {
-                    'user_id': request.user.id
-                });
-
-            cache.client()
-                .set('layer', body)
-                .then(function(layer){
-                    var compositionData = {
-                        'layer_id': layer.id,
-                        'group_id': groupId
-                    };
-                    cache.client()
-                        .set('composition', compositionData)
-                        .then(function(/* composition */){
-                            response.status(201).send(layer);
-                        });
-                })
-                .catch(function(err){
-                    response.status(500).send(err);
-                });
-        },
+        put: {
+            verb: 'put',
+            handler: 'put',
+            url: 'user/:user_id/group/:group_id/layer/:layer_id',
+            permissions: ['isAuthenticated', 'isLayerOwner']
+        }
+    },
 
 
-        put: function (request, response) {
-            var layerId = request.params.layer_id,
-                body = _.extend(request.body, {
+    list: function (request, response) {
+        var self = this;
+        cache.client()
+            .getLayers(request.params.group_id)
+            .then(function(results){
+                self.paginate(results, request, response);
+            })
+            .catch(function(err){
+                response.status(500).send(err);
+            });
+    },
+
+    get: function (request, response) {
+        cache.client()
+            .get('layer', request.params.layer_id)
+            .then(function(data){
+                response.send(data);
+            })
+            .catch(function(err){
+                response.status(404).send(err);
+            });
+    },
+
+
+    post: function (request, response) {
+        var groupId = request.params.group_id,
+            body = _.extend(request.body, {
+                'user_id': request.user.id
+            });
+
+        cache.client()
+            .set('layer', body)
+            .then(function(layer){
+                var compositionData = {
+                    'layer_id': layer.id,
+                    'group_id': groupId
+                };
+                cache.client()
+                    .set('composition', compositionData)
+                    .then(function(/* composition */){
+                        response.status(201).send(layer);
+                    });
+            })
+            .catch(function(err){
+                response.status(500).send(err);
+            });
+    },
+
+
+    put: function (request, response) {
+        var layerId = request.params.layer_id,
+            body = _.extend(request.body, {
                 'user_id': request.user.id,
                 'id': layerId
             });
-            cache.client()
-                .set('layer', body)
-                .then(function(data){
-                    response.send(data);
-                    notifier.update('layer', layerId, data);
-                })
-                .catch(function(err){
-                    response.status(500).send(err);
-                });
-        },
+        cache.client()
+            .set('layer', body)
+            .then(function(data){
+                response.send(data);
+                notifier.update('layer', layerId, data);
+            })
+            .catch(function(err){
+                response.status(500).send(err);
+            });
+    },
 
 
-    });
+});
